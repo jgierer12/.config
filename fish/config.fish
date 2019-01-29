8882a7577cc74646a0da1a8771da3383d922a32c
Jonas Gierer <jgierer12@gmail.com>
2019-01-29 18:00:43 +0100

    Add comments to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
+# Set config folder if unset
 set -q XDG_CONFIG_HOME; or set -x XDG_CONFIG_HOME ~/.config
 
+# Change fisher path so vendor functions aren't mixed up with custom ones
 set -g fisher_path $XDG_CONFIG_HOME/fish/vendor
 
 set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
@@ -9,19 +11,23 @@ for file in $fisher_path/conf.d/*.fish
   builtin source $file 2> /dev/null
 end
 
+# Add custom bins and npm bins to PATH
 for p in ~/.bin (yarn bin --offline) (yarn global bin --offline)
   if test -d $p
     set PATH $PATH $p
   end
 end
 
+# Add home, dev, desktop to CDPATH for fast navition
 for p in . ~ ~/dev ~/Desktop
   if test -d $p
     set CDPATH $CDPATH $p
   end
 end
 
+# Set editor to VSCode
 set -x VISUAL code
 set -x EDITOR $VISUAL
 
+# Use ripgrep for fzf
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --smart-case --glob "!.git/*"'
