7a37eb1a849169c15f4c54e8e7c1ee74ef907722
Jonas Gierer <jgierer12@gmail.com>
2019-02-11 15:09:36 +0100

    Add git abbr subheadings

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -3,50 +3,60 @@ function add
 end
 
 # git
+## Fast commit
 add f "git fast"
 add ff "git fast fastcommit"
 
+## Commit
 add gc "git commit"
 add gca "git commit --amend --no-edit"
 
+## Add
 add ga "git add"
 add gaa "git add --all"
 add gap "git add --patch"
 
-add gm "git merge"
-
-add grb "git rebase"
+## Reset
 add gr "git reset"
 add grh "git reset HEAD"
 add grr "git reset HEAD --hard"
 
+## Status
 add gs "git status"
 add gsl "git status --long"
 
+## Branch
 add gb "git branch --verbose"
 add gbr "git branch --verbose --remote"
 add gbd "git branch --delete"
 
+## Checkout (go)
 add gg "git checkout"
 add ggm "git checkout master"
 add ggb "git checkout -b"
 
+## Diff
 add gd "git diff"
 add gdt "git difftool"
 add gdc "git conflicts"
 
+## Log
 add gl "git changelog"
 add gll "git changelog \"@{upstream}\".."
 add glu "git changelog ..\"@{upstream}\""
 add glb "git changelog master.."
 add glg "git graph"
 
+## Push/Pull
 add gp "git push"
 add gpu "git push --set-upstream"
 add gpl "git pull"
 
+## Misc
 add gcl "git clone"
 add gfe "git fetch"
+add gm "git merge"
+add grb "git rebase"
 
 # npm
 add nr "npm run"
