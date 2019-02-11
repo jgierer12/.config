17945e160d9176a43a42be538058ecc94c179922
Jonas Gierer <jgierer12@gmail.com>
2019-02-11 15:01:08 +0100

    Add sections in git abbrs

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -5,33 +5,43 @@ end
 # git
 add f "git fast"
 add ff "git fast fastcommit"
+
 add gc "git commit"
 add gca "git commit --amend --no-edit"
+
 add ga "git add"
 add gaa "git add --all"
 add gap "git add --patch"
+
 add gm "git merge"
+
 add grb "git rebase"
 add gr "git reset"
 add grh "git reset HEAD"
 add grr "git reset HEAD --hard"
+
 add gs "git status"
 add gsl "git status --long"
+
 add gg "git checkout"
 add ggb "git checkout -b"
 add ggm "git checkout master"
+
 add gd "git diff"
 add gdt "git difftool"
 add gdc "git conflicts"
+
 add gl "git changelog"
 add gll "git changelog \"@{upstream}\".."
 add glu "git changelog ..\"@{upstream}\""
 add glb "git changelog master.."
 add glg "git graph"
 add gcl "git clone"
+
 add gp "git push"
 add gpu "git push --set-upstream"
 add gpl "git pull"
+
 add gfe "git fetch"
 
 # npm
