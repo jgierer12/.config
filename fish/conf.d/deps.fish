c9660f9b58cdd1f711da49a936319e1f5d48eebd
Jonas Gierer <jgierer12@gmail.com>
2019-03-08 20:55:06 +0100

    Refactor `deps_install_all`/`deps_save_all`

--- fish/conf.d/deps.fish
+++ fish/conf.d/deps.fish
@@ -24,6 +24,22 @@ function _deps_log_section
   log_section cyan $argv
 end
 
+function _deps_for_all
+  set action $argv[1]
+  for name in system vscode yarn_global fish
+    _deps_log_section $name
+    deps_$action\_$name
+  end
+end
+
+function deps_install_all
+  _deps_for_all install
+end
+
+function deps_save_all
+  _deps_for_all save
+end
+
 # Install
 function deps_install_system
   if type --quiet yay; and test -z "$SKIP_SYSTEM_DEPS"
@@ -45,13 +61,6 @@ function deps_install_fish
   fisher add (_deps_get fish)
 end
 
-function deps_install_all
-  for name in system vscode yarn_global fish
-    _deps_log_section $name
-    deps_install_$name
-  end
-end
-
 # Save
 function deps_save_system
   _deps_set system (yay --query --explicit --quiet)
@@ -70,11 +79,4 @@ end
 
 function deps_save_fish
   _deps_set fish (fisher ls)
-end
-
-function deps_save_all
-  for name in system vscode yarn_global fish
-    _deps_log_section $name
-    deps_save_$name
-  end
 end
