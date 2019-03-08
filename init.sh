e2d968764662dee86f8759403b8fce911eff6e5f
Jonas Gierer <jgierer12@gmail.com>
2019-03-08 19:39:20 +0100

    Add init script

---
+++ init.sh
@@ -0,0 +1,18 @@
+if [ -z "$XDG_CONFIG_HOME" ] ; then
+  XDG_CONFIG_HOME="$HOME/.config"
+fi
+
+cd $XDG_CONFIG_HOME
+
+git init
+git remote add origin https://github.com/jgierer12/.config.git
+git fetch
+git checkout --force --track origin/master
+
+if command -v pacman > /dev/null && [ -z "$SKIP_SYSTEM_DEPS" ] ; then
+  sudo pacman --sync yay fish
+fi
+
+if command -v fish > /dev/null; then
+  fish --command 'deps_install_system; sysupdate'
+fi
