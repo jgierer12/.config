a6265bb7fb6d88d2bea4289b81b5bfb60ae58f6a
Jonas Gierer <jgierer12@gmail.com>
2019-03-29 19:05:53 +0100

    Remove unused system deps

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,3 @@ set --query XDG_CONFIG_HOME; or set --export XDG_CONFIG_HOME ~/.config
 set --export VISUAL code
 set --export EDITOR $VISUAL
 
-# Use ripgrep for fzf
-set --export FZF_DEFAULT_COMMAND 'rg --files --hidden --smart-case --glob "!.git/*"'
