214efee925fbcd45f5e0649b2119880b7faeaa5a
Jonas Gierer <jgierer12@gmail.com>
2019-04-08 15:05:26 +0200

    Add fish keybinding for quick `j`

--- fish/conf.d/keybindings.fish
+++ fish/conf.d/keybindings.fish
@@ -1,2 +1,14 @@
 # Workaround for https://github.com/fish-shell/fish-shell/issues/3537
 bind \cc 'commandline ""'
+
+function _keybinding_j
+  set cl (commandline)
+  if test -z "$cl"
+    # If the command line is empty, insert `j `
+    commandline 'j '
+  else
+    # Otherwise, append `j`
+    commandline $cl'j'
+  end
+end
+bind j _keybinding_j
