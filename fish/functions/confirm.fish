7c57eb8a7ad9c93cd301cd1578d8d6e424e54c04
Jonas Gierer <jgierer12@gmail.com>
2019-04-22 13:20:50 +0200

    Add `confirm` function

---
+++ fish/functions/confirm.fish
@@ -0,0 +1,11 @@
+function confirm
+  while true
+    read --local confirm --prompt-str "$argv [Y/n] "
+    switch $confirm
+      case N n
+        return 1
+      case '' Y y
+        return 0
+    end
+  end
+end
