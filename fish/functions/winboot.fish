9554cec7f8f0cb6a6052d7464b4e921af6d8be3d
Jonas Gierer <jonas@gierer.xyz>
2019-05-05 18:14:56 +0200

    Remove winboot function
    I'm no longer dual-booting windows

--- fish/functions/winboot.fish
+++
@@ -1,7 +0,0 @@
-function winboot
-  set windows (sudo grep -i "^menuentry 'Windows" /boot/grub/grub.cfg|head -n 1|cut -d"'" -f2)
-  echo Rebooting into $windows
-
-  sudo grub-reboot $windows
-  sudo reboot
-end
