f5ea014b690dd90e913757c26db1b715ccdb0d3c
Jonas Gierer <jonas@gierer.xyz>
2019-06-01 19:22:50 +0200

    Add `dl` function
    Wrapper around `youtube-dl` to serve my most common
    needs:
    
    - Dedicated audio/video sub-commands
    - Ability to add cookies in ~/.config/cookies.txt
    - Auto numbers

---
+++ fish/functions/dl.fish
@@ -0,0 +1,17 @@
+function dl
+  set format $argv[1]
+  set rest_args $argv[2..-1]
+
+  switch $format
+    case audio a
+      _dl_base --format bestaudio --extract-audio $rest_args
+    case video v
+      _dl_base $rest_args
+    case '*'
+      _dl_base $argv
+  end
+end
+
+function _dl_base
+  youtube-dl --cookies $XDG_CONFIG_HOME/youtube-dl/cookies.txt --output '%(autonumber)s - %(title)s.%(ext)s' $argv
+end
